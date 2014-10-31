2813ec827bcb396ee4f5f3fbedd8a538411fd820
Cody Bumgardner <cbumgardner@gmail.com>
2014-10-31 10:20:25 -0400

    Second Major Push

---
+++ src/main/java/channels/MsgInQueue.java
@@ -0,0 +1,55 @@
+package channels;
+
+import shared.MsgEvent;
+import shared.MsgEventType;
+import core.AgentEngine;
+import core.CommandExec;
+
+
+public class MsgInQueue implements Runnable {
+
+	private CommandExec commandExec;
+	
+	public MsgInQueue()
+	{
+		commandExec = new CommandExec();
+	}
+	
+	public void run() 
+	{
+
+		AgentEngine.MsgInQueueEnabled = true;
+		while(AgentEngine.MsgInQueueEnabled)
+		{
+			try
+			{
+				if(AgentEngine.MsgInQueueActive) //process queue if active
+				{
+					synchronized(AgentEngine.msgInQueue) 
+					{
+			    		while ((!AgentEngine.msgInQueue.isEmpty()) && AgentEngine.MsgInQueueEnabled) 
+			    		{
+			    			MsgEvent me = AgentEngine.msgInQueue.poll(); //get logevent
+			    			
+			    				new MsgRoute(me).run(); //route messages in new thread
+			    		}
+					}
+				}
+				Thread.sleep(100);
+			}
+			catch(Exception ex)
+			{
+				System.out.println("Agent : MsgInQueue Error :" + ex.getMessage());
+			}
+			
+		}
+		//shutdown was called
+		
+	}
+
+	
+	
+
+
+}
+
---
+++ src/main/java/channels/MsgOutQueue.java
@@ -0,0 +1,36 @@
+package channels;
+
+import core.AgentEngine;
+
+
+public class MsgOutQueue implements Runnable {
+
+	
+	public MsgOutQueue()
+	{
+	}
+	public void run() 
+	{
+
+		AgentEngine.MsgOutQueueEnabled = true;
+		while(AgentEngine.MsgOutQueueEnabled)
+		{
+			try
+			{
+				Thread.sleep(1000);
+				if(AgentEngine.MsgOutQueueActive) //process queue if active
+				{
+					
+				}
+			}
+			catch(Exception ex)
+			{
+				System.out.println("Agent : MsgOutQueue Error :" + ex.getMessage());
+			}
+			
+		}
+		//shutdown was called
+		
+	}
+}
+
