d9dec8800c3bded3060f86cccc34b1c751ab9b50
Caylin Hickey <caylin.hickey@uky.edu>
2016-07-19 17:38:15 -0400

    Changing from maxFilesize to maxHistory in logback configuration

--- src/main/java/channels/MsgRoute.java
+++ src/main/java/channels/MsgRoute.java
@@ -21,7 +21,7 @@ public class MsgRoute implements Runnable {
             }
 
             int routePath = getRoutePath();
-            if (rm.getMsgType() != MsgEvent.Type.WATCHDOG && rm.getMsgType() != MsgEvent.Type.LOG && rm.getMsgType() != MsgEvent.Type.KPI) {
+            if (rm.getMsgType() != MsgEvent.Type.WATCHDOG && rm.getMsgType() != MsgEvent.Type.LOG && rm.getMsgType() != MsgEvent.Type.KPI && rm.getMsgType() != MsgEvent.Type.INFO) {
                 logger.debug("Routing: Path={}, Type={}, Src={}-{}:{}, Dst={}-{}:{}, Params={}", routePath,
                         rm.getMsgType().name(),
                         rm.getParam("src_region"), rm.getParam("src_agent"), rm.getParam("src_plugin"),
