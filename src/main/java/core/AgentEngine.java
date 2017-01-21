fd6ceb291954635eff3228a29bff64a77fbaaa0c
Cody Bumgardner <cody@uky.edu>
2017-01-21 10:34:09 -0500

    Add ability to change global controller static discovery address

--- src/main/java/core/AgentEngine.java
+++ src/main/java/core/AgentEngine.java
@@ -248,6 +248,11 @@ public class AgentEngine {
 
     public static void LoadControllerPlugin() throws InterruptedException {
         controllerPluginSlot = "plugin/0";
+
+        //THIS NEEDS TO BE MOVED!!!
+        String globalControllerHost = System.getenv("CRESCO_GC_HOST");
+        pluginsconfig.getPluginConfig("plugin/0").addProperty("gc_host",globalControllerHost);
+
         boolean isComm = enablePlugin(controllerPluginSlot, false);
         if (!isComm) {
             System.out.println("failed to load");
